beb5fc7be5eab274f1857fc77046a6e4c43c44b2
vittorio <vittoriovta@gmail.com>
2023-11-11 23:07:08 +0100

    installed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# carlaACCsimulation, a simulator for ACC systems, built around CARLA simulator
+#     Copyright (C) 2023  Vittorio Folino Rocco Depietra Giuseppe Sansevero
+
+#     This program is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 import carla
 import random
 import time
